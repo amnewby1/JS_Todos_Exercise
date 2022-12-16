413042ed69aaffad2ac1c6caa7d2f98f02b4f9db
amnewby1 <114841649+amnewby1@users.noreply.github.com>
2022-12-15 22:11:33 -0500

    Add files via upload

---
+++ JS_Todos_Exercise.js
@@ -0,0 +1,41 @@
+const form = document.querySelector("#todos");
+const toDoInput = document.querySelector("#New_Todo");
+const toDoList = document.querySelector("#todo_list");
+
+const savedToDos = JSON.parse(localStorage.getItem("savedToDos"));
+if (savedToDos) {
+  savedToDos.forEach((task) => {
+    if (!task.isDone) {
+      const newToDo = document.createElement("li");
+      const doneBtn = document.createElement("button");
+      doneBtn.innerText = "Done";
+      newToDo.innerText = toDoInput.value;
+      newToDo.appendChild(doneBtn);
+      toDoList.appendChild(newToDo);
+    }
+  });
+}
+
+form.addEventListener("submit", function (e) {
+  const savedToDos = JSON.parse(localStorage.getItem("savedToDos"));
+  e.preventDefault();
+  const newToDo = document.createElement("li");
+  const doneBtn = document.createElement("button");
+  doneBtn.innerText = "Done";
+  newToDo.innerText = toDoInput.value;
+  newToDo.appendChild(doneBtn);
+  toDoList.appendChild(newToDo);
+
+  savedToDos.push({ taskName: toDoInput.value, isDone: false });
+  console.log(savedToDos);
+  localStorage.setItem("savedToDos".JSON.stringify(savedToDos));
+  toDoInput.value = "";
+});
+
+toDoList.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.style.textDecoration = "line-through";
+  } else if (e.target.tagName === "BUTTON") {
+    e.target.parentElement.remove();
+  }
+});
